fd042027ed2a064f5e2498d5a2275c9c9f8ed486
elta79 <earnold@outlook.com>
2024-04-16 18:01:11 -0400

    organize images

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,14 +14,14 @@ function Body(){
                         <a href="#" className="p-2 font-semibold tracking-widest border-t-2 border-l-2 bg-amber-400 text-amber-950 border-amber-300">Contact Me</a>
                     </div>                    
                 </div>  
-                <img src="../public/fullPiano.jpg" className="lg:w-1/2" />
+                <img src="../public/images/fullPiano.jpg" className="lg:w-1/2" />
             </section>
 
             <AnchorSection id="lessons" />
             <section className="flex flex-col items-center justify-center border-2 border-red-500 h-dvh">
                 <h2 className="pb-8 text-4xl text-stone-200 ">Lesson Information</h2>
                 <div className="flex flex-col lg:flex-row">                    
-                    <img src="../public/sheetmusic.jpg" className="p-6 lg:w-1/2" /> 
+                    <img src="../public/images/sheetmusic.jpg" className="p-6 lg:w-1/2" /> 
                     <div>
                         <h3 className="px-6 pt-6 text-3xl text-stone-300 lg:leading-relaxed">Offering In-Home Lessons</h3>       
                         <ul>
@@ -41,7 +41,7 @@ function Body(){
             <section className="flex flex-col items-center justify-center border-2 border-red-600 h-dvh">
                 <h2 className="pb-6 text-4xl text-stone-200">About Earl</h2>
                 <div className="flex flex-col lg:flex-row">
-                    <img src="../public/earl.png" className="w-2/3 m-auto my-6 border-8 border-stone-600"/>  
+                    <img src="../public/images/earl.png" className="w-2/3 m-auto my-6 border-8 border-stone-600"/>  
                     <div className>
                     <p className="px-6 pt-6 text-lg text-stone-200">Earl boasts over two decades of experience as a music educator. He earned his Bachelor&apos;s degree in music education with a vocal emphasis from Florida A&M University in 2002, followed by a Master&apos;s degree in Music Education from the University of Florida in 2010.</p>
                     <p className="px-6 pt-6 text-lg text-stone-200">With a remarkable tenure of 35 years, Earl has been instrumental as a music director in various ministries. His expertise extends to directing music for numerous productions, including recent successes such as <q>Sistas the Musical</q>, <q>Avenue Q</q>, and <q>The Color Purple</q>.</p>
